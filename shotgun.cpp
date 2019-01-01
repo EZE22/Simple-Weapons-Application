8df04ba2fbabefe3800b6076e95e34eaeac3dee3
Adam Ingram <37667723+EZE22@users.noreply.github.com>
2019-01-01 15:27:15 -0500

    shotgun.cpp

---
+++ shotgun.cpp
@@ -0,0 +1,13 @@
+/******************
+* Adam E. Ingram *
+* Date: 1/1/19   *
+******************/
+
+#include"shotgun.h"
+
+void Shotgun::draw() {
+	cout << "You just drew your shotgun!!!" << endl;
+}
+void Shotgun::reload() {
+	cout << "You just shot your shotgun and now you are reloading!!!\n" << endl;
+}
