00d6b6b27a7fac2ae782702018b7fd0e32bbadf7
Adam Ingram <37667723+EZE22@users.noreply.github.com>
2019-01-01 15:29:22 -0500

    crossbow.cpp

---
+++ crossbow.cpp
@@ -0,0 +1,13 @@
+/******************
+* Adam E. Ingram *
+* Date: 1/1/19   *
+******************/
+
+#include"crossbow.h"
+
+void Crossbow::draw() {
+	cout << "You just drew back you crossbow!!!!" << endl;
+}
+void Crossbow::reload() {
+	cout << "You just shot your crossbow and you are now reloading!!!!\n" << endl; 
+}
