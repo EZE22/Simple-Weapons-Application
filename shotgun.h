789e10fb9df970c586ff540e7e1c1ad6898b50e8
Adam Ingram <37667723+EZE22@users.noreply.github.com>
2019-01-01 15:28:04 -0500

    shotgun.h

---
+++ shotgun.h
@@ -0,0 +1,12 @@
+/******************
+* Adam E. Ingram *
+* Date: 1/1/19   *
+******************/
+
+#pragma once
+#include"weapons.h"
+
+class Shotgun :public weapons {
+	void draw();
+	void reload();
+};
